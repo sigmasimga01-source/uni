87b9e67f3a0613eea46d30b706f215449161c3ad
NeRo <tazo.gorgadze.02@gmail.com>
2026-01-17 01:09:25 +0400

    fix

--- _private/app/index.php
+++ _private/app/index.php
@@ -1,12 +1,2 @@
 <?php
 header("Location: views/index.php");
-# Source - https://stackoverflow.com/a
-# Posted by alexn
-# Retrieved 2026-01-08, License - CC BY-SA 3.0
-
-RewriteEngine On
-RewriteCond %{REQUEST_FILENAME} !-f
-RewriteCond %{REQUEST_FILENAME} !-d
-RewriteRule ^(.*)$ $1.php [L]
-
-exit();
