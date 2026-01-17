9fd0e36da67e8acc846a42fd6376371f19c0447e
NeRo <tazo.gorgadze.02@gmail.com>
2026-01-18 01:00:28 +0400

    feat: add admin tabs partial for navigation in admin panel

---
+++ app/views/admin/_partials/tabs.php
@@ -0,0 +1,4 @@
+    <div class="admin-tabs">
+      <a href="products.php" class="admin-tab <?php if ($a === 1) echo 'active'; ?>">Products</a>
+      <a href="orders.php" class="admin-tab <?php if ($a === 2) echo 'active'; ?>">Orders</a>
+    </div>
